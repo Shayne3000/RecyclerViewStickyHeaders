bddf197f41b835ec2d894e09fb5ef644fe706b0e
Unknown <alfred3000prime@hotmail.com>
2017-09-19 17:47:54 +0300

    Adapter positioning functions in progress.

--- library/src/main/java/com/senijoshua/library/StickyHeaderSectionAdapter.java
+++ library/src/main/java/com/senijoshua/library/StickyHeaderSectionAdapter.java
@@ -18,7 +18,6 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
     private HashMap<Integer, GroupSelectionState> selectionStateByGroup = new HashMap<>();
     private HashMap<Integer, SectionSelectionState> selectionStateBySection = new HashMap<>();
     private int[] groupIndicesByAdapterPosition;
-    private int[] sectionIndicesByAdapterPosition;
     private int totalNumberOfItems;
     private Handler mainThreadHandler;
 
@@ -113,7 +112,7 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
         }
     }
 
-    public static class ItemViewHolder extends SectionViewHolder{
+    public static class ItemViewHolder extends SectionViewHolder {
         private int itemPositionInSection;
 
         public ItemViewHolder(View itemView) {
@@ -154,7 +153,6 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
     }
 
 
-
     //Override this function to set the view type for the group header
     //The value you return here will be passes to onCreateItemViewHolder and onBindItemViewHolder as the viewType
     public int getGroupHeaderViewType(int groupIndex) {
@@ -165,17 +163,18 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
     //Override this function to set the view type for the group header
     //The value you return here will be passes to onCreateItemViewHolder and onBindItemViewHolder as the viewType
     //groupIndex- the section's group
-    public int getSectionHeaderViewType(int groupIndex, int sectionIndex){
+    public int getSectionHeaderViewType(int groupIndex, int sectionIndex) {
         return 0;
     }
 
-    public int getSectionItemViewType(int groupIndex, int sectionIndex, int itemIndex){
+    public int getSectionItemViewType(int groupIndex, int sectionIndex, int itemIndex) {
         return 0;
     }
 
     /**
      * This is used to create a ViewHolder for an itemView in a given section.
-     * @param parent The ViewGroup into which the new view will be added after it's  been bound to an adapter position.
+     *
+     * @param parent       The ViewGroup into which the new view will be added after it's  been bound to an adapter position.
      * @param itemViewType If getSectionItemViewType is overridden to return custom types, this will be the specified type
      * @return a new ItemViewHolder holding an itemView
      */
@@ -185,7 +184,8 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
 
     /**
      * This creates a ViewHolder for the Section header view
-     * @param parent The ViewGroup that section header view would be added to after it's been bound to an adapter position.
+     *
+     * @param parent                The ViewGroup that section header view would be added to after it's been bound to an adapter position.
      * @param sectionHeaderViewType if getSectionHeaderViewType is overriden to return custom types, this will be the specified type
      * @return a new SectionHeaderViewHolder containing a GroupHeaderView
      */
@@ -195,7 +195,8 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
 
     /**
      * This is called when a ViewHolder is required for the Group header view
-     * @param parent The ViewGroup that the group header view would be added to after it's been bound to an adapter position.
+     *
+     * @param parent              The ViewGroup that the group header view would be added to after it's been bound to an adapter position.
      * @param groupHeaderViewType if getSectionHeaderViewType is overriden to return custom types, this will be the specified type.
      * @return a new GroupHeaderViewHolder containing a GroupHeaderView
      */
@@ -205,13 +206,14 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
 
     /**
      * Binds item data to the view at a specified position
-     * @param holder the holder of the view that would be bound to the item data
-     * @param groupIndex index of the group within which the item resides
+     *
+     * @param holder       the holder of the view that would be bound to the item data
+     * @param groupIndex   index of the group within which the item resides
      * @param sectionIndex index of the section that holds the item
-     * @param itemIndex index of the concened item in the section
+     * @param itemIndex    index of the concened item in the section
      * @param itemViewType the type of the view which could be a user-provided custom type through overriding getItemViewType
      */
-    public void onBindItemViewHolder(ItemViewHolder holder, int groupIndex, int sectionIndex, int itemIndex, int itemViewType){
+    public void onBindItemViewHolder(ItemViewHolder holder, int groupIndex, int sectionIndex, int itemIndex, int itemViewType) {
 
     }
     //This is called when you want to set a section header view at a particular position
@@ -220,22 +222,24 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
 
     /**
      * This binds header data to section header view for a section
-     * @param holder the holder of the section header view that would be updated
-     * @param groupIndex index of the group within which the section header view resides
-     * @param sectionIndex index of the section that contains the header view
+     *
+     * @param holder                the holder of the section header view that would be updated
+     * @param groupIndex            index of the group within which the section header view resides
+     * @param sectionIndex          index of the section that contains the header view
      * @param sectionHeaderViewType the type of the view that could be a user-supplied custom type through overriding getSectionHeaderViewType
      */
-    public void onBindSectionHeaderViewHolder(SectionHeaderViewHolder holder, int groupIndex, int sectionIndex, int sectionHeaderViewType){
+    public void onBindSectionHeaderViewHolder(SectionHeaderViewHolder holder, int groupIndex, int sectionIndex, int sectionHeaderViewType) {
 
     }
 
     /**
      * This binds the respective header data to the group header view for a group
-     * @param holder the holder of the group header view that would be updated
-     * @param groupIndex the index of the group containing the header to update
+     *
+     * @param holder              the holder of the group header view that would be updated
+     * @param groupIndex          the index of the group containing the header to update
      * @param groupHeaderViewType the type of the view which could be a custom type set by the user through overriding getGroupHeaderViewType
      */
-    public void onBindGroupHeaderViewHolder(GroupHeaderViewHolder holder, int groupIndex, int groupHeaderViewType){
+    public void onBindGroupHeaderViewHolder(GroupHeaderViewHolder holder, int groupIndex, int groupHeaderViewType) {
 
     }
 
@@ -245,13 +249,18 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
         int numberOfSections;   // number of items including section header + section items excluding group header
         int length;             // total number of items in groups including  group header, sections and all its items
         boolean isStickyHeader;      // if true, the group is at the top of the list i.e sticky header
+        private int[] sectionIndicesByGroupPosition; // An array that holds the indices of the group's contents at each position
+        // i.e. section 0 & item 0 in that section would both have indices of 0
+        // at positions 0 and 1 respectively.
     }
 
 
     private static class Section {
         int adapterPosition;    // adapterPosition of first item (the header) of this section
+        int groupIndex;         // this denotes the group this section belongs to where 0 is the first group in the list.
         int numberOfItems;      // number of items in the section excluding the section header. (Ideally this would be length - 1)
         int length;             // total number of items in sections including header and footer
+        int indexInGroup;       // This denotes the index of the section in the group where 0 is the first section in the group
         boolean isStickyHeader;      // if true, the section is at the top of the list under the group header
     }
 
@@ -267,7 +276,6 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
     }
 
     /**
-     *
      * @param groupIndex the index of the concerned group
      * @return true if the group has been scrolled off the visible screen or hidden.
      */
@@ -280,11 +288,11 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
     }
 
     /**
-     *
-     * @param sectionIndex the index of the concerned section
+     * @param groupIndex   the index of the containing group
+     * @param sectionIndex the index of the concerned section within the group
      * @return true if the section has been scrolled off the visible screen or hidden.
      */
-    public boolean isSectionHidden(int sectionIndex) {
+    public boolean isSectionHidden(int sectionIndex, int groupIndex) {
         if (hiddenSections.containsKey(sectionIndex)) {
             return hiddenSections.get(sectionIndex);
         }
@@ -299,45 +307,60 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
         return 0;
     }
 
+    public int getNumberOfItemsInSectionWithinGroup(int groupIndex) {
+        return 0;
+        //have a global count
+        //Iterate through all the sections and for each section that has groupIndex as groupIndex
+        //add the numberofItemsCount to the global count
+        //Then return the global count.
+    }
+
     /**
-     * @param groupIndex index of the concerned group
+     * @param groupIndex index of the concerned group where the first item starts with index 0
      * @return the number of sections in the specified group. A possible count could be the number of section headers.
      */
     public int getNumberOfSectionsInGroup(int groupIndex) {
         return 0;
+        //have a global count
+        //Iterate through all the sections and for each section that has groupIndex as groupIndex
+        //increment the global count
+        //return the global count
     }
 
     /**
-     * @param sectionIndex index of the concerned section or no of items in a section, given a group index
+     * @param sectionIndex index of the concerned section within a group
+     * @param groupIndex   index of the group that contains the concerned section
      * @return the number of items in the specified section
      */
-    public int getNumberOfItemsInSection(int sectionIndex) {
+    public int getNumberOfItemsInSection(int sectionIndex, int groupIndex) {
         return 0;
     }
 
 
     /**
-     *
      * @param GroupIndex index of the concerned group
      * @return if this group is a header at the top of the list.
      */
-    public boolean isGroupSticky(int GroupIndex){
+    public boolean isGroupSticky(int GroupIndex) {
+        return false;
+    }
+
+    /**
+     * @param groupIndex   index of the group where the respective section is located
+     * @param sectionIndex index of the concerned section within a group
+     * @return if this section is a header at the top of the list under the group's sticky header.
+     */
+    public boolean isSectionSticky(int sectionIndex, int groupIndex) {
         return false;
     }
 
     /**
      *
-     * @param sectionIndex index of the concerned section
-     * @return if this section is a header at the top of the list under the group's sticky header.
+     * @param adapterPosition the position in the adapter which ranges from 0 to getItemCount() - 1
+     * @return the index of the group that corresponds to the adapter position where 0 is the first group in the list
      */
-    public boolean isSectionSticky(int sectionIndex){
-        return false;
-    }
-
-    //Given a "global" adapter adapterPosition, determine which group contains that item
-    public int getGroupforAdapterPosition(int adapterPosition){
+    public int getGroupforAdapterPosition(int adapterPosition) {
         if (groups == null) {
-            buildSectionIndex();
             buildGroupIndex();
         }
 
@@ -352,7 +375,13 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
         return groupIndicesByAdapterPosition[adapterPosition];
     }
 
-    public int getSectionInGroupForAdapterPosition(int adapterPosition){
+    /**
+     *
+     * @param groupIndex
+     * @param adapterPosition
+     * @return the index of the section that a certain pos
+     */
+    public int getSectionInGroupForAdapterPosition(int groupIndex, int adapterPosition) {
 
     }
 
@@ -370,11 +399,11 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
                 group.length = 0;
                 group.numberOfSections = getNumberOfSectionsInGroup(groupIndex);
             } else {
-                group.length = group.numberOfSections + getNumberOfItemsInSection(groupIndex);
+                group.length = group.numberOfSections + getNumberOfItemsInSectionWithinGroup(groupIndex);
             }
 
             if (group.isStickyHeader) {
-                group.length += 1; // room for header
+                group.length += 1; // room for f
             }
 
             this.groups.add(group);
@@ -385,39 +414,43 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
         totalNumberOfItems = i;
 
         i = 0;
-        sectionIndicesByAdapterPosition = new int[totalNumberOfItems];
-        for (int s = 0, ns = getNumberOfSections(); s < ns; s++) {
-            Section section = sections.get(s);
-            for (int p = 0; p < section.length; p++) {
-                sectionIndicesByAdapterPosition[i + p] = s;
+        groupIndicesByAdapterPosition = new int[totalNumberOfItems];
+        for (int groupIndex = 0, groupCount = getNumberOfGroups(); groupIndex < groupCount; groupIndex++) {
+            Group group = groups.get(groupIndex);
+            for (int pos = 0; pos < group.length; pos++) {
+                groupIndicesByAdapterPosition[i + pos] = groupIndex;
             }
+            i += group.length;
 
-            i += section.length;
+        }
+        for (int groupIndex = 0, groupSize = getNumberOfGroups(); groupIndex < groupSize; groupIndex++) {
+            buildSectionIndex(groupIndex, groupIndicesByAdapterPosition);
         }
     }
 
-    private void buildSectionIndex() {
+    private void buildSectionIndex(int groupIndex, int[] groupIndicesByAdapterPosition) {
+        int totalNumberOfItemsInGroup;
+        Group group = groups.get(groupIndex);
         sections = new ArrayList<>();
 
         int i = 0;
-        for (int s = 0, ns = getNumberOfSections(); s < ns; s++) {
+        for (int sectionIndexInGroup = 0, sectionCount = getNumberOfSectionsInGroup(groupIndex);
+             sectionIndexInGroup < sectionCount; sectionIndexInGroup++) {
             Section section = new Section();
-            section.adapterPosition = i;
-            section.hasHeader = doesSectionHaveHeader(s);
-            section.hasFooter = doesSectionHaveFooter(s);
+            section.adapterPosition = group.adapterPosition + i + 1;
+            section.groupIndex = groupIndex;
+            section.indexInGroup = sectionIndexInGroup;
+            section.isStickyHeader = isSectionSticky(sectionIndexInGroup, groupIndex);
 
-            if (isSectionCollapsed(s)) {
+            if (isSectionHidden(sectionIndexInGroup, groupIndex)) {
                 section.length = 0;
-                section.numberOfItems = getNumberOfItemsInSection(s);
+                section.numberOfItems = getNumberOfItemsInSection(sectionIndexInGroup, groupIndex);
             } else {
-                section.length = section.numberOfItems = getNumberOfItemsInSection(s);
+                section.length = section.numberOfItems = getNumberOfItemsInSection(sectionIndexInGroup, groupIndex);
             }
 
-            if (section.hasHeader) {
-                section.length += 2; // room for header and ghostHeader
-            }
-            if (section.hasFooter) {
-                section.length++;
+            if (section.isStickyHeader) {
+                section.length += 1; // room for header
             }
 
             this.sections.add(section);
@@ -425,14 +458,15 @@ public class StickyHeaderSectionAdapter extends RecyclerView.Adapter<StickyHeade
             i += section.length;
         }
 
-        totalNumberOfItems = i;
+        totalNumberOfItemsInGroup = i;
 
         i = 0;
-        sectionIndicesByAdapterPosition = new int[totalNumberOfItems];
-        for (int s = 0, ns = getNumberOfSections(); s < ns; s++) {
-            Section section = sections.get(s);
-            for (int p = 0; p < section.length; p++) {
-                sectionIndicesByAdapterPosition[i + p] = s;
+        group.sectionIndicesByGroupPosition = new int[totalNumberOfItemsInGroup];
+        for (int sectionIndexInGroup = 0, sectionCount = getNumberOfSectionsInGroup(groupIndex);
+             sectionIndexInGroup < sectionCount; sectionIndexInGroup++) {
+            Section section = sections.get(sectionIndexInGroup);
+            for (int pos = 0; pos < section.length; pos++) {
+                group.sectionIndicesByGroupPosition[i + pos] = sectionIndexInGroup;
             }
 
             i += section.length;
